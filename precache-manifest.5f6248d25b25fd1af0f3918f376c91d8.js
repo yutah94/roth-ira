7f887ccb0e2e461924c6ff152dfdc5beb77d0f3e
Yuta <yuta.young@gmail.com>
2020-01-27 12:48:59 -0700

    Updates

---
+++ precache-manifest.5f6248d25b25fd1af0f3918f376c91d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddc69c80b1def7c1b2b4cf7c053c901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af84fbc5d4f935e4d0d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "21626f7ab9bb8ebd1504",
+    "url": "/static/js/2.6ce3c245.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6ce3c245.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4af84fbc5d4f935e4d0d",
+    "url": "/static/js/main.274e65b1.chunk.js"
+  },
+  {
+    "revision": "c76e41ce9d06dd885972",
+    "url": "/static/js/runtime-main.6d3d5393.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
